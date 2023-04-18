0418febde80b443a374ca4815df2f6ae5bf47538
mangeshwayaldeveloper <120046874+mangeshwayaldeveloper@users.noreply.github.com>
2023-04-18 17:55:19 +0530

    final and constant

---
+++ secondflutter/lib/finalVsConst.dart
@@ -0,0 +1,9 @@
+void main(){
+  final mangesh;
+  mangesh=12;
+  print(mangesh);
+ // mangesh=10;   ---> this can't be done because of final
+ /*final var name;//fin we also can not write
+  name=10; */
+
+}
