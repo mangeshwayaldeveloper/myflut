bb0a9b11a3561614bebb087f6c7c900b19f2fea9
Mangesh Wayal <mangesh.wayal.dypimr@gmail.com>
2023-08-31 22:24:21 +0530

    setting data in widgets

--- lib/Screens/home.dart
+++ lib/Screens/home.dart
@@ -19,6 +19,7 @@ class _HomePageState extends State<HomePage> {
         await http.get(Uri.parse('https://jsonplaceholder.typicode.com/posts'));
     var data = jsonDecode(response.body.toString());
     if (response.statusCode == 200) {
+      postList.clear();
       for (Map i in data) {
         postList.add(PostModel.fromJson(i));
       }
@@ -35,12 +36,45 @@ class _HomePageState extends State<HomePage> {
         title: Text("Demo API"),
         centerTitle: true,
       ),
-      body: Container(
-        child: Column(
-          children: [
-
-          ],
-        ),
+      body: Column(
+        children: [
+          Expanded(
+            child: FutureBuilder(
+                future: getPostApi(),
+                builder: (context, snapshot) {
+                  if (!snapshot.hasData) {
+                    return Text("loading data...");
+                  } else {
+                    return ListView.builder(
+                        itemCount: postList.length,
+                        itemBuilder: (context, index) {
+                          return Card(
+                            child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    "Title\n " +
+                                        postList[index].title.toString(),
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 17),
+                                  ),
+                                  Text(
+                                    postList[index].body.toString(),
+                                    style: TextStyle(color: Colors.black54),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          );
+                        });
+                  }
+                }),
+          )
+        ],
       ),
     );
   }
