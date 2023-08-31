d23ad9477830c1d1f05730f509431eb295bfbe07
Mangesh Wayal <mangesh.wayal.dypimr@gmail.com>
2023-08-31 22:05:40 +0530

    setting api connection

--- lib/Screens/home.dart
+++ lib/Screens/home.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
+
+import 'package:demoapi/Models/postModel.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -8,12 +12,35 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  List<PostModel> postList = [];
+
+  Future<List<PostModel>> getPostApi() async {
+    final response =
+        await http.get(Uri.parse('https://jsonplaceholder.typicode.com/posts'));
+    var data = jsonDecode(response.body.toString());
+    if (response.statusCode == 200) {
+      for (Map i in data) {
+        postList.add(PostModel.fromJson(i));
+      }
+      return postList;
+    } else {
+      return postList;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Demo API"),
         centerTitle: true,
+      ),
+      body: Container(
+        child: Column(
+          children: [
+
+          ],
+        ),
       ),
     );
   }
